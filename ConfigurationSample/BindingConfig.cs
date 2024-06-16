e2bb18ea9d00a9bb925ef9a7dbed3fc0d701bab6
daohainam <daohainam@gmail.com>
2024-06-16 10:46:12 +0200

    Add ConfigurationSample project

---
+++ ConfigurationSample/BindingConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationSample
+{
+    internal class BindingConfig
+    {
+        public string StringConfig { get; set; } = string.Empty;
+        public int IntegerConfig { get; set; }
+        public bool BoolConfig { get; set; }
+
+    }
+}
---
+++ ConfigurationSample/Program.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConfigurationSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("ConfigFiles/mysettings.json", optional: false)
+                .AddJsonFile("ConfigFiles/mysettings.optional.json", optional: true)
+                .AddXmlFile("ConfigFiles/mysettings.xml")
+                .AddKeyPerFile("ConfigFiles/KeyPerFile", optional: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            PrintConfiguredProviders(configuration);
+            Console.WriteLine();
+
+            PrintConfigValues(configuration);
+            Console.WriteLine();
+
+            var config = configuration.GetRequiredSection("JsonConfigSample").Get<BindingConfig>();
+            Print(config);
+        }
+
+        private static void Print(BindingConfig? config)
+        {
+            if (config == null)
+            {
+                Console.WriteLine("config == null");
+            }
+            else
+            {
+                Console.WriteLine($"StringConfig: {config.StringConfig}");
+                Console.WriteLine($"IntegerConfig: {config.IntegerConfig}");
+                Console.WriteLine($"BoolConfig: {config.BoolConfig}");
+            }
+        }
+
+        private static void PrintConfiguredProviders(IConfigurationRoot configuration)
+        {
+            foreach (var p in configuration.Providers)
+            {
+                Console.WriteLine($"Provider: {p.GetType().Name}");
+            }
+        }
+
+        private static void PrintConfigValues(IConfigurationRoot configuration)
+        {
+            foreach (var config in configuration.AsEnumerable())
+            {
+                Console.WriteLine($"{config.Key} = {config.Value}");
+            }
+        }
+    }
+}
