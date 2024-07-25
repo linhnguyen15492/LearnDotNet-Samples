f6e4acbe015a5ee28bc8e195b3eb0bf8f5d93560
daohainam <daohainam@gmail.com>
2024-07-25 16:16:28 +0200

    Add Chat programs

---
+++ Chat/ChatClient/Program.cs
@@ -0,0 +1,68 @@
+﻿using ChatProtocol;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatClient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var endPoint = new IPEndPoint(IPAddress.Loopback, ChatProtocol.Constants.DefaultChatPort);
+
+            var clientSocket = new Socket(
+                endPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+
+            await clientSocket.ConnectAsync(endPoint);
+            var buffer = new byte[1024];
+            var r = await clientSocket.ReceiveAsync(buffer);
+            if (r == 0)
+            {
+                showConnectionError();
+                return;
+            }
+
+            var welcomeText = Encoding.UTF8.GetString(buffer, 0, r);
+            if (!Constants.WelcomeText.Equals(welcomeText))
+            {
+                showConnectionError();
+                return;
+            }
+
+            Console.WriteLine(welcomeText);
+
+            while (true)
+            {
+                Console.Write("Enter your message: ");
+                var msg = Console.ReadLine();
+
+                if (string.IsNullOrEmpty(msg)) {
+                    await closeConnectionAsync(clientSocket);
+                    return;
+                }
+                else
+                {
+                    var bytes = Encoding.UTF8.GetBytes(msg);
+                    await clientSocket.SendAsync(bytes);
+                }
+            }
+        }
+
+        private static async Task closeConnectionAsync(Socket clientSocket)
+        {
+            var bytes = Encoding.UTF8.GetBytes(Constants.CommandShutdown);
+            await clientSocket.SendAsync(bytes);
+
+            clientSocket.Close();   
+        }
+
+        private static void showConnectionError()
+        {
+            Console.WriteLine("Invalid protocol!");
+        }
+    }
+}
---
+++ Chat/ChatServer/Program.cs
@@ -0,0 +1,72 @@
+﻿using ChatProtocol;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ChatServer
+{
+    internal class Program
+    {
+
+
+        static async Task Main(string[] args)
+        {
+            int clientId = 1;
+
+            var endPoint = new IPEndPoint(IPAddress.Loopback, ChatProtocol.Constants.DefaultChatPort);
+            var serverSocket = new Socket(
+                endPoint.AddressFamily,
+                SocketType.Stream,
+                ProtocolType.Tcp
+                );
+
+            serverSocket.Bind(endPoint);
+
+            Console.WriteLine($"Listening... (port {endPoint.Port})");
+
+            serverSocket.Listen();
+
+            var clientHandlers = new List<Task>();
+
+            while (true)
+            {
+                var clientSocket = await serverSocket.AcceptAsync();
+                var t = handleClientRequestAsync(clientSocket, clientId++);
+                clientHandlers.Add(t);
+            }
+
+            Task.WaitAll([.. clientHandlers]);
+       }
+
+        private static async Task handleClientRequestAsync(Socket clientSocket, int clientId)
+        {
+            Console.WriteLine($"[Client {clientId}] connected!");
+
+            var welcomeBytes = Encoding.UTF8.GetBytes(Constants.WelcomeText);
+            await clientSocket.SendAsync(welcomeBytes);
+
+            var buffer = new byte[1024];
+
+            while (true)
+            {
+                var r = await clientSocket.ReceiveAsync(buffer);
+                var msg = Encoding.UTF8.GetString(buffer, 0, r);
+
+                if (msg.Equals(Constants.CommandShutdown))
+                {
+                    closeConnection(clientSocket);
+                    Console.WriteLine($"[Client {clientId}] disconnected!");
+                    break;
+                }
+
+                Console.WriteLine($"[Client {clientId}]: {msg}");
+            }
+        }
+
+        private static void closeConnection(Socket clientSocket)
+        {
+            clientSocket.Close();
+        }
+    }
+}
